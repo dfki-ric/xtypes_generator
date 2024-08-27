d4957cea4870951ba8ec6301f8d9ddd50dc769b8
Priyanka Chowdhury <46909017+Priyanka328@users.noreply.github.com>
2024-08-27 14:42:21 +0200

    Update prepare_build.sh

--- .circleci/prepare_build.sh
+++ .circleci/prepare_build.sh
@@ -3,4 +3,4 @@ export DEBIAN_FRONTEND=noninteractive
 
 # Update and install dependencies
 apt-get update
-apt-get install -y build-essential git cmake pkg-config  libncurses5-dev python3 python3-jinja2 python3-yaml python3-pip
+apt-get install -y build-essential git cmake pkg-config  libncurses5-dev python3 python3-jinja2 python3-yaml python3-pip libboost-all-dev
