38ed5e3705707cec1bcfe55d203fa88c746c0e08
Priyanka Chowdhury <46909017+Priyanka328@users.noreply.github.com>
2024-08-27 18:39:36 +0000

    added github ci

--- .circleci/prepare_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEBIAN_FRONTEND=noninteractive
-
-# Update and install dependencies
-apt-get update
-apt-get install -y build-essential git cmake pkg-config  libncurses5-dev python3 python3-jinja2 python3-yaml python3-pip
